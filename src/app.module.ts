d8b4e257721c0c1613dcff995a8cca0b12f0af9f
tsimafei.kinevich <tsimafei.kinevich@itrexgroup.com>
2023-11-17 12:03:44 +0300

    feat(env validation)!: Add env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { validate } from './utilities/config/env.validation';
 
 @Module({
   imports: [
     UserModule,
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ validate, isGlobal: true }),
     DatabaseModule,
   ],
 })
---
+++ src/utilities/config/env.validation.ts
@@ -0,0 +1,36 @@
+import { plainToClass } from 'class-transformer';
+import { IsNumber, IsString, validateSync } from 'class-validator';
+
+class EnvironmentVariables {
+  @IsNumber()
+  PORT: number;
+
+  @IsString()
+  DB_HOST: string;
+
+  @IsNumber()
+  DB_PORT: number;
+
+  @IsString()
+  DB_NAME: string;
+
+  @IsString()
+  DB_USERNAME: string;
+
+  @IsString()
+  DB_PASSWORD: string;
+}
+
+export function validate(configuration: Record<string, unknown>) {
+  const finalConfig = plainToClass(EnvironmentVariables, configuration, {
+    enableImplicitConversion: true,
+  });
+
+  const errors = validateSync(finalConfig);
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return finalConfig;
+}
